0cb992ee3256d15b63189958feed07e54d571a78
aalvarez11 <aalvarez1@email.arizona.edu>
2024-10-30 14:29:33 -0700

    update functions to use database

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -10,6 +10,7 @@ let jobs = [
 
 // GET ALL
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
@@ -23,48 +24,32 @@ export const createJob = async (req, res) => {
 // GET JOB
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-
+  const job = await Job.findById(id);
+  // null check
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-
   res.status(200).json({ job });
 };
 
 // EDIT JOB
 export const updateJob = async (req, res) => {
-  // check request body for company and position fields
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
-  // check if a job with the id exists
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-
-  if (!job) {
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
+  // null check
+  if (!updatedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-
-  // both checks pass so update job info
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
+  res.status(200).json({ msg: 'job modified', job: updatedJob });
 };
 
 // DELETE JOB
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-
-  if (!job) {
+  const removedJob = await Job.findByIdAndDelete(id);
+  // null check
+  if (!removedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-
-  res.status(200).json({ msg: 'job deleted' });
+  res.status(200).json({ msg: 'job deleted', job: removedJob });
 };
