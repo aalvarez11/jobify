2899a7d601e83c18bd6d5bd8b1abcd39ea7d591d
aalvarez11 <aalvarez1@email.arizona.edu>
2024-10-31 07:52:20 -0700

    add error cases for 400, 401 and 403

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,9 +1,37 @@
 import { StatusCodes } from 'http-status-codes';
 
+// 404 case
 export class NotFoundError extends Error {
   constructor(message) {
     super(message);
     this.name = 'NotFoundError';
     this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
+// 400 case
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+// 401 case
+export class UnauthenticatedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthenticatedError';
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+// 403 case
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = StatusCodes.FORBIDDEN;
   }
 }
