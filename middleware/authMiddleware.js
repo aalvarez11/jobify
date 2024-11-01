427feb0b8d280f18344a1c6dedcf6d168a5db1e2
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-01 13:08:59 -0700

    fix token destructuring

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,14 @@
 import { UnauthenticatedError } from '../errors/customErrors.js';
 import { verifyJWT } from '../utils/tokenUtils.js';
 
-export const authenticateUser = (req, res, next) => {
+export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) throw new UnauthenticatedError('authentication invalid');
+  if (!token) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
 
   try {
-    const data = verifyJWT(token);
+    const { userId, role } = verifyJWT(token);
     req.user = { userId, role };
     next();
   } catch (error) {
