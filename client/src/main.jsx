c3169b5fa764b84b09f937753b1744d80bf0d1ae
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-02 14:28:00 -0700

    restructure + add toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import './index.css';
 import customFetch from './utils/customFetch.js';
 
 const data = await customFetch.get('/test');
 console.log(data);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+    <ToastContainer position='top-center' />
+  </React.StrictMode>
 );
