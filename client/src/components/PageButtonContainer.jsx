0111696ccde5eb95ef68adcb495b7021ef356339
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-06 13:07:51 -0700

    add rendering of page numbers

--- client/src/components/PageButtonContainer.jsx
+++ client/src/components/PageButtonContainer.jsx
@@ -7,7 +7,35 @@ const PageButtonContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
-  console.log(numOfPages + ' ' + currentPage);
-  return <h1>PageButtonContainer</h1>;
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  console.log(pages);
+
+  return (
+    <Wrapper>
+      <button className='btn prev-btn'>
+        <HiChevronDoubleLeft /> prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+              key={pageNumber}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className='btn next-btn'>
+        <HiChevronDoubleRight /> next
+      </button>
+    </Wrapper>
+  );
 };
 export default PageButtonContainer;
