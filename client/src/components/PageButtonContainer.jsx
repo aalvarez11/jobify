3b16370d56a4d21f18766a42afac2e7d35d3a42a
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-06 13:30:35 -0700

    add page navigation via buttons

--- client/src/components/PageButtonContainer.jsx
+++ client/src/components/PageButtonContainer.jsx
@@ -2,6 +2,7 @@ import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 import Wrapper from '../assets/wrappers/PageBtnContainer';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 import { useAllJobsContext } from '../pages/AllJobs';
+import { handle } from 'express/lib/router';
 
 const PageButtonContainer = () => {
   const {
@@ -11,11 +12,27 @@ const PageButtonContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1;
   });
-  console.log(pages);
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+    console.log(pageNumber);
+  };
 
   return (
     <Wrapper>
-      <button className='btn prev-btn'>
+      <button
+        className='btn prev-btn'
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) prevPage = 1;
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft /> prev
       </button>
       <div className='btn-container'>
@@ -26,13 +43,23 @@ const PageButtonContainer = () => {
                 pageNumber === currentPage && 'active'
               }`}
               key={pageNumber}
+              onClick={() => {
+                handlePageChange(pageNumber);
+              }}
             >
               {pageNumber}
             </button>
           );
         })}
       </div>
-      <button className='btn next-btn'>
+      <button
+        className='btn next-btn'
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numOfPages) nextPage = numOfPages;
+          handlePageChange(nextPage);
+        }}
+      >
         <HiChevronDoubleRight /> next
       </button>
     </Wrapper>
