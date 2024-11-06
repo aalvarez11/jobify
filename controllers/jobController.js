dc66e6b5fa77bfd0ecfcdfb235e0e3e31f57223b
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-05 22:25:46 -0700

    add pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -35,8 +35,21 @@ export const getAllJobs = async (req, res) => {
   };
   const sortKey = sortOptions[sort] || sortOptions.newest;
 
-  const jobs = await Job.find(queryObject).sort(sortKey);
-  res.status(StatusCodes.OK).json({ jobs });
+  // set up pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 // CREATE JOB
@@ -67,6 +80,7 @@ export const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
 };
 
+// SHOW STATS
 export const showStats = async (req, res) => {
   // grab all jobs that belong to the user, then GROUP BY the job status (dec, int, pend)
   let stats = await Job.aggregate([
