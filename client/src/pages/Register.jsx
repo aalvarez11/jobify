b65c704fba59b2629b15e466a6daefd8e2d8dfe7
aalvarez11 <aalvarez1@email.arizona.edu>
2024-11-07 09:53:29 -0700

    remove form default vals

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,16 +24,11 @@ const Register = () => {
       <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
-        <FormRow type='text' name='name' defaultValue='Alice' />
-        <FormRow
-          type='text'
-          name='lastName'
-          labelText='last name'
-          defaultValue='P'
-        />
-        <FormRow type='text' name='location' defaultValue='earth' />
-        <FormRow type='email' name='email' defaultValue='alicep@gmail.com' />
-        <FormRow type='password' name='password' defaultValue='secret123' />
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
         <SubmitButton />
         <p>
           Already a member?
